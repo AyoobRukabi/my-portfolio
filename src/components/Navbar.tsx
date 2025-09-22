abfedffe6d2c664fc7c2f6c9bd4992a942587a3c
mac <mac@MacBookAir.mshome.net>
2025-09-22 15:52:11 +0300

    Add Education

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,6 +43,7 @@ const Navbar: React.FC = () => {
     { name: "Skills", href: "#skills" },
     { name: "Projects", href: "#projects" },
     { name: "Experience", href: "#experience" },
+    { name: "Education", href: "#education" },
     { name: "Contact", href: "#contact" },
   ];
 
